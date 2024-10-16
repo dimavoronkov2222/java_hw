9eb9aa55d6d48d619344469c65acbb25fd836d35
dimavoronkov2222 <dimavoronkov2222@hotmail.com>
2024-10-16 16:38:04 +0200

    edit code

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //1. Выведите на экран надпись "Your time is limited, so
+        //don’t waste it living someone else’s life" Steve Jobs на разных
+        //строках.
+        String sentence1 = "Your time is limited,";
+        String sentence2 = "so don’t waste it";
+        String sentence3 = "living someone else’s life";
+        String sentence4 = "Steve Jobs";
+        System.out.println(sentence1 + "\n" + sentence2 + "\n" + sentence3 + "\n" + sentence4 + "\n end task #1");
+
+    }
+}
